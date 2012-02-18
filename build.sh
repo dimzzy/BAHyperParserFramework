75df97f7a07276fb7ad8e9c32ea01ed9076f9cb7
Dmitry Stadnik <dimzzy@gmail.com>
2012-02-18 15:09:58 +0100

    Update xcodebuild path

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ export OUTPUT=$WORKSPACE/output
 rm -rf $OUTPUT
 mkdir -p $OUTPUT
 
-/Developer/usr/bin/xcodebuild -target BAHyperParser -configuration Release build || failed build;
+xcodebuild -target BAHyperParser -configuration Release build || failed build;
 
 cd $WORKSPACE/build/Release-iphoneuniversal || failed output;
 rm BAHyperParser
