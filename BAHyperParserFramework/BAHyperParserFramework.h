a8d5ea864fa1c16e5e91c22a622ee197748d4eaa
Dmitry Stadnik <dimzzy@gmail.com>
2012-02-18 08:29:00 +0100

    Initial Commit

---
+++ BAHyperParserFramework/BAHyperParserFramework.h
@@ -0,0 +1,13 @@
+//
+//  BAHyperParserFramework.h
+//  BAHyperParserFramework
+//
+//  Created by Stadnik Dmitry on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BAHyperParserFramework : NSObject
+
+@end
---
+++ BAHyperParserFramework/BAHyperParserFramework.m
@@ -0,0 +1,13 @@
+//
+//  BAHyperParserFramework.m
+//  BAHyperParserFramework
+//
+//  Created by Stadnik Dmitry on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BAHyperParserFramework.h"
+
+@implementation BAHyperParserFramework
+
+@end
